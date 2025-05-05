61073a63b5c1a00c2867a30bca898726bbf87868
Giovanni Manfredi <manfredi2000@hotmail.it>
2025-05-06 00:03:04 +0200

    Domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/GiovanniManfredi/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
